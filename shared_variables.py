2c5737035a468aea1c7fb11bbded9099e1ebe20f
JBorrow <joshua.borrow@durham.ac.uk>
2018-10-15 14:46:12 +0100

    Added parameter file to the repository

--- shared_variables.py
+++ shared_variables.py
@@ -2,4 +2,13 @@
 Shared variables throughout scripts.
 """
 
+import yaml
+
 environment_name = "simba_env"
+
+try:
+    with open("parameters.yml", "r") as f:
+        parameters = yaml.load(f)
+except FileNotFoundError:
+    pass
+
