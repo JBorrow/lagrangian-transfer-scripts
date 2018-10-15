683478a3cfa62125bbf2b419384adc55b81b24cb
JBorrow <joshua.borrow@durham.ac.uk>
2018-10-15 14:50:42 +0100

    Fixed parameter file and created object to parse the yaml

--- shared_variables.py
+++ shared_variables.py
@@ -6,9 +6,28 @@ import yaml
 
 environment_name = "simba_env"
 
+class Parameters(object):
+    def __init__(self, handle):
+        self.raw_data = yaml.load(handle)
+
+        self.parse()
+
+        return
+
+    def parse(self):
+        self.snapshot_ini = self.raw_data["snapshots"]["ini"]
+        self.snapshot_end = self.raw_data["snapshots"]["end"]
+
+        self.halo_catalogue_type = self.raw_data["halo_catalogue"]["type"]
+        self.halo_catalogue_run_caesar = self.raw_data["halo_catalogue"]["run_caesar"]
+        self.halo_catalogue_path = self.raw_data["halo_catalogue"]["path"]
+        self.halo_catalogue_pickle_name = self.raw_data["halo_catalogue"]["pickle_name"]
+
+        return
+    
+        
 try:
     with open("parameters.yml", "r") as f:
-        parameters = yaml.load(f)
+        parameters = Parameters(f)
 except FileNotFoundError:
     pass
-
